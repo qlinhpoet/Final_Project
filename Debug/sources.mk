8505ff80851d43c0b7730f4163d524e580943246
Huyphann155 <huyphan1505dut@gmail.com>
2022-11-26 20:29:10 +0700

    Speed read with encoder

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -1,5 +1,6 @@
 ################################################################################
 # Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
 ################################################################################
 
 ELF_SRCS := 
@@ -10,18 +11,18 @@ S_UPPER_SRCS :=
 O_SRCS := 
 SIZE_OUTPUT := 
 OBJDUMP_LIST := 
+SU_FILES := 
 EXECUTABLES := 
 OBJS := 
+MAP_FILES := 
 S_DEPS := 
 S_UPPER_DEPS := 
 C_DEPS := 
-OBJCOPY_BIN := 
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-Src/app \
-Src/driver \
-Src \
-Src/middleware \
-Startup \
+Core/Src \
+Core/Startup \
+Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates \
+Drivers/STM32F4xx_HAL_Driver/Src \
 
